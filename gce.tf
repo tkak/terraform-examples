bc0cd0926392976aac4897a9870bbb3b3b55d6d3
tkak <takaaki.frkw@gmail.com>
2014-10-05 14:27:19 +0900

    try to use terraform google cloud plugin

---
+++ gce.tf
@@ -0,0 +1,22 @@
+provider "google" {
+    account_file = "./account.json"
+    client_secrets_file = "./client_secrets.json"
+    project = "tkakfrkw"
+    region = "asia-east1-a"
+}
+
+/*
+resource "google_compute_instance" "default" {
+    name = "test"
+    machine_type = "f1-micro"
+    zone = "asia-east1-b"
+
+    disk {
+        image = "debian-7-wheezy-v20140814"
+    }
+
+    network {
+        source = "default"
+    }
+}
+*/
