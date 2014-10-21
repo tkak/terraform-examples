a4cbab264711f81ff5f74f1123f937afb52cd38f
tkak <takaaki.frkw@gmail.com>
2014-10-21 12:11:37 +0900

    test depends_on

---
+++ demo5/conoha.tf
@@ -0,0 +1,14 @@
+variable "conoha_user" {}
+variable "conoha_password" {}
+variable "conoha_tenant" {}
+
+provider "conoha" {
+    user = "${var.conoha_user}"
+    password = "${var.conoha_password}"
+    tenant = "${var.conoha_tenant}"
+}
+
+resource "conoha_container" "example" {
+    name = "test1"
+    depends_on = ["google_compute_instance.default"]
+}
